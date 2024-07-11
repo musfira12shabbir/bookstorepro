ef0f29d32c4dc33b60904954927c32da0163bad8
huzaifa-44 <huzaifaarshad197@gmail.com>
2024-07-11 23:04:18 +0500

    abc

---
+++ lib/home_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Home Page'),
+    Text('Search Page'),
+    Text('Profile Page'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bookstore'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+            },
+          ),
+        ],
+      ),
+
+      drawer: Drawer(
+
+      ),
+
+
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_books),
+            label: 'Library',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+
+import 'dart:async';
+
+import 'package:bookstoreproject/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState 
+    Timer(
+      const Duration(milliseconds: 2000),()=>Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen(),))
+    );
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+         width: double.infinity,
+
+          decoration: const BoxDecoration(
+             color: Colors.grey,
+            image: DecorationImage(
+
+              image: NetworkImage("https://static.vecteezy.com/system/resources/thumbnails/032/325/096/small/bookshelf-isolated-on-transparent-background-file-cut-out-ai-generated-png.png"),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
